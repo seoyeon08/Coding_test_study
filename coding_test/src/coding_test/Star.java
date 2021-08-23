3ddccad2d1e88a1547a898e59b4e073c98185cdf
seoyeon <silversun0808@gmail.com>
2021-08-23 16:55:24 +0900

    star fin

--- coding_test/src/coding_test/Star.java
+++ coding_test/src/coding_test/Star.java
@@ -1,10 +1,18 @@
 package coding_test;
+import java.util.Scanner;
 
 public class Star {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner (System.in);
+		int n = scan.nextInt(); //가로
+		int m = scan.nextInt(); //세로
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
 	}
 
 }
