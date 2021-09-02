46a6b108caca3ae3864730240c065258577a4a19
seoyeon <silversun0808@gmail.com>
2021-09-02 19:25:45 +0900

    add watermelon

---
+++ coding_test/src/coding_test/lev 1/Watermelon.java
@@ -0,0 +1,16 @@
+package coding_test;
+
+public class Watermelon {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++){
+            if (i % 2 == 0){
+                answer += "¼ö";
+            }
+            else {
+                answer += "¹Ú";
+            }
+        }
+        return answer;
+    }
+}
