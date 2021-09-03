e0a228e9ed480e4720527e74c43a68d368b1a504
seoyeon <silversun0808@gmail.com>
2021-09-04 02:55:55 +0900

    finish Phoneketmon

--- coding_test/src/coding_test/lev 1/Phoneketmon.java
+++ coding_test/src/coding_test/lev 1/Phoneketmon.java
@@ -2,9 +2,27 @@ package coding_test;
 
 public class Phoneketmon {
 
-	public static void main(String[] args) {
+	public static int main(int[] nums) {
 		// TODO Auto-generated method stub
-
+		int answer = 0;
+        int [] arr = new int[200001];   //200000으로 하면 런타임 에러;;
+        for(int i = 0; i < nums.length; i++){
+            if(arr[nums[i]] == 0){      //없으면
+                answer ++;
+            }
+            arr[nums[i]]++;
+        }
+        if (answer > nums.length / 2){
+            answer = nums.length / 2;
+        }
+        return answer;
 	}
 
+}
+
+
+class Solution {
+    public int solution(int[] nums) {
+        
+    }
 }
